5eb4f43e0822bd699c8183c82900acaad0d2f4d1
rajat <rajat2411gupta@gmail.com>
2021-05-05 09:09:58 +0530

    implement socket.io-client

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Auth () {
+function Auth () {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
     <div>
-    auth
+      <div>
+        <h1>Join</h1>
+        <div>
+          <input placeholder="username" className="" type="text" onChange={(e) => setName(e.target.value)} />
+          <input placeholder="roomname" className="" type="text" onChange={(e) => setRoom(e.target.value)} />
+        </div>
+        <Link onClick={ e => (!name || !room ) && e.preventDefault() } to={`/chat?name=${name}&room=${room}`}>
+          <button>Sign In</button>
+        </Link>
+      </div>
     </div>
   )
 }
+
+export default Auth;
--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import queryString from "query-string";
 
-export default function Chat () {
+let socket;
+
+function Chat ({ location }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io.connect(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+  }, [location.search, ENDPOINT])
+
   return (
     <div>
-      chat
+      Chat
     </div>
   )
 }
+
+export default Chat;
